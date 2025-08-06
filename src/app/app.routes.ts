b1702c19866de1940a781c1a9f8a083709a0e1f8
Tomaz <tomazcraft@gmail.com>
2025-08-06 08:06:05 -0300

    feat: inicializa projeto com Angular standalone, TailwindCSS e rotas básicas
    - Cria projeto com suporte a standalone
    - Configura TailwindCSS para estilização utilitária
    - Adiciona rotas lazy-loading para auth, dashboard e transactions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+
+    },
+    {
+        path: 'auth',
+        loadComponent: () => import('./features/auth/auth/auth.component').then(m => m.AuthComponent)
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    },
+    {
+        path: 'transactions',
+        loadComponent: () => import('./features/transactions/transactions/transactions.component').then(m => m.TransactionsComponent)
+    },
+    {
+        path: '**',
+        redirectTo: '/dashboard',
+    }
+];
