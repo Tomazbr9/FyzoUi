c370882b71a0a2453091c09207d2e68994a4be73
Tomaz <tomazcraft@gmail.com>
2025-08-08 10:21:31 -0300

    feat(auth): templates customizados para melhor experiencia do usuário

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -6,8 +6,8 @@ export class AuthGuard {
     constructor(private router: Router) {}
 
     canActivate(): boolean {
-        const token = localStorage.getItem('token');
-        
+        const token = localStorage.getItem('access_token');
+
         if (!token) {
             this.router.navigate(['/login']);
             return false;
